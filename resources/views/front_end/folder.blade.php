4959058456df7833abd86ead60c76bcb56f43a2e
Fadi frejat <fadi.alfrejat@gmail.com>
2025-06-22 18:40:12 +0300

    add filter to folders+

--- resources/views/front_end/folder.blade.php
+++ resources/views/front_end/folder.blade.php
@@ -89,7 +89,7 @@
                                 <div class="row g-4">
                                     @foreach ($videos as $vd)
                                         <div class="col-md-4">
-                                            <iframe width="100%" height="240" src="{{$vd->link}}" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                            <iframe width="100%" height="240" style="border-radius: 20px" src="{{$vd->link}}" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                         </div>
                                     @endforeach
                                 </div>
